eae261617ebafc67fff98509c57fcff60dc2770e
anaisfloch <anaisflochlycee@gmail.com>
2024-10-28 17:05:09 +0100

    ajout carte leaflet

--- core-master/views/accueil.php
+++ core-master/views/accueil.php
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
 <html lang="fr">
+
     <head>
         <meta charset="UTF-8">
         <title>Accueil</title>
-        <link rel="stylesheet" href="assets/js.css">
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>        
+        <link rel="stylesheet" href="assets/jeu.css">
+        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>        <script src="https://cdn.jsdelivr.net/npm/vue"></script>
     </head>
+
     <body>
         <h1>TEST</h1>
-        <p><?php echo "Bonjour"; ?></p>
+        <div id="map"></div>
+        <footer id="inventaire"></footer>
         <script src="assets/jeu.js"></script>
     </body>
+
 </html>
