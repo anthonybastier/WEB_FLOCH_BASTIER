9febc740e90a6f8e5bf16c746510f767d29c6643
anaisfloch <anaisflochlycee@gmail.com>
2024-10-28 15:54:13 +0100

    ajout script js et css

--- core-master/views/accueil.php
+++ core-master/views/accueil.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
     <head>
         <meta charset="UTF-8">
         <title>Accueil</title>
+        <link rel="stylesheet" href="assets/js.css">
     </head>
     <body>
         <h1>TEST</h1>
         <p><?php echo "Bonjour"; ?></p>
+        <script src="assets/jeu.js"></script>
     </body>
 </html>
