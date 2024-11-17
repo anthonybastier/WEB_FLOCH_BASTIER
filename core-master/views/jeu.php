0ae8bf4950d2dce87386a4fce508199a7d21a86e
anaisfloch <anaisflochlycee@gmail.com>
2024-11-17 13:11:03 +0100

    modif id du footer et réorganisation

--- core-master/views/jeu.php
+++ core-master/views/jeu.php
@@ -6,19 +6,21 @@
         <title>Escape Death</title>
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>        
         <link rel="stylesheet" href="assets/jeu.css">
-        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>        <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+        <script src="https://cdn.jsdelivr.net/npm/vue"></script>
     </head>
 
     <body>
         <h1>Escape Death</h1>
-        <div id="map"></div>
-        <h1>Liste des objets</h1>
-        <div id="app">
-        <ul>
-            <li v-for="objet in list_obj" :key="objet.id">{{objet.nom}}
-        </ul>
+        <div id="map">
+            <my-component></my-component>
         </div>
-        <footer id="inventaire"></footer>
+        <footer id="inventaire">
+            <h3>Inventaire</h3>
+            <ul>
+                <li v-for="objet in list_obj" :key="objet.id">{{objet.nom}}
+            </ul>
+        </footer>
         <script src="assets/jeu.js"></script>
     </body>
 
