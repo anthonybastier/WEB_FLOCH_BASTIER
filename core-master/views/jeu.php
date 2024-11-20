76ac75f9aeb23d1788571df6baca6d8266bbcf3e
Scarfluix <anthony-bstr@outlook.fr>
2024-11-20 13:19:15 +0100

    bugfix

--- core-master/views/jeu.php
+++ core-master/views/jeu.php
@@ -8,19 +8,20 @@
         <link rel="stylesheet" href="assets/jeu.css">
         <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
         <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+        <link rel="icon" href="assets/sprite/death.ico">
     </head>
 
     <body>
         <h1>Escape Death</h1>
         <div id="appmap">
-            <my-component></my-component>
+            <div id="map"></div>
+            <footer id="inventaire">
+                <h3>Inventaire</h3>
+                <ul>
+                    <li v-for="objet in tab_obj" :key="objet.id">{{ objet.nom }}</li>
+                </ul>
+            </footer>
         </div>
-        <footer id="inventaire">
-            <h3>Inventaire</h3>
-            <ul>
-                <li v-for="objet in list_obj" :key="objet.id">{{objet.nom}}
-            </ul>
-        </footer>
         <script src="assets/jeu.js"></script>
     </body>
 
