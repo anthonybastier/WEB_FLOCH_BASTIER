0ed93c81f9b131c06658091dd34167128081d396
anaisfloch <anaisflochlycee@gmail.com>
2024-11-21 21:09:02 +0100

    changement méthode d'ajout de pseudo

--- core-master/assets/accueil.js
+++ core-master/assets/accueil.js
@@ -3,11 +3,39 @@ Vue.createApp({
       return {
         pseudo: '',
         max: 12,
+        message: '',
+        success: false
       };
     },
     methods: {
-        identification() {
-            return(this.liste.push(this.pseudo));}
+        async play() {
+    		try {
+        		const response = await fetch('/addpseudobdd', {
+            		method: 'POST',
+            		headers: {
+                		'Content-Type': 'application/json'
+            		},
+            		body: JSON.stringify({
+                		pseudo: this.pseudo
+            		})
+        		});
+
+        		const data = await response.json();
+        		console.log('Données reçues:', data);
+
+        		this.message = data.message;
+        		this.success = data.success;
+
+        		if (this.success) {
+        		    this.pseudo = '';
+        		    window.location.href = '/jeu';
+        		}
+    		} catch (error) {
+        		this.message = "Erreur lors de l'ajout du pseudo.";
+        		this.success = false;
+        		console.error('Fetch Error:', error);
+    		}
+		}
         },
     computed: {
         nbrCaracRestants() {
